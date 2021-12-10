62ab785c9fee5a0e6d619b4ae3a32767852febf5
Tejas Athni <63206545+tathni@users.noreply.github.com>
2021-12-10 14:25:55 -0500

    Updated .csv and shell

--- 5b-ecoregions-intersect.sh
+++ 5b-ecoregions-intersect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export IFS=","
+export IFS = ","
 
 cat 5c-species-list.csv | while read a; do
 
-job_file="species${a}_ecoregions_intersect.job"
+job_file = "species${a}_ecoregions_intersect.job"
 
 
 echo "#!/bin/bash
@@ -11,12 +11,13 @@ echo "#!/bin/bash
 #SBATCH  -p normal
 #SBATCH --job-name=spec${a}_ecoregions
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1      
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --output species${a}_ecoregions_intersect.log
 
 
+ml physics gdal udunits proj geos
 ml R/4.0.2
 
 Rscript ./5a-ecoregions.R "$a" " > $job_file
